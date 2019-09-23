75719eb047d087abe4d60e7e77417278d82f6077
bibibic <star054321@naver.com>
2019-09-23 16:49:15 +0900

    add email, tag models

--- emptytime/notice/admin.py
+++ emptytime/notice/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from django.contrib import admin
 # models에서 BlogData를 import 해옵니다.
-from .models import TitleData
+from .models import TitleData, EmailAccount
 
 # 아래의 코드를 입력하면 BlogData를 admin 페이지에서 관리할 수 있습니다.
 admin.site.register(TitleData)
+admin.site.register(EmailAccount)
--- emptytime/notice/models.py
+++ emptytime/notice/models.py
@@ -5,3 +5,12 @@ class TitleData(models.Model):
     title = models.CharField(max_length=500)
     def __str__(self):
         return self.title
+class EmailAccount(models.Model):
+    myEmail = models.CharField(max_length=50)
+    def __str__(self):
+        return self.myEmail
+class Mytag():
+    tag = models.CharField(max_length=50)
+    emailAccount=models.ForeignKey(EmailAccount,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.tag
