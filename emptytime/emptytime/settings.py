3822d161f955f329ee663feef317a00ed0355ad3
bibibic <star054321@naver.com>
2019-09-28 00:49:07 +0900

    email_sending

--- emptytime/emptytime/settings.py
+++ emptytime/emptytime/settings.py
@@ -99,7 +99,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = 'kookmin.notice@gmail.com'
+EMAIL_HOST_PASSWORD = 'moominvalley@1200'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
