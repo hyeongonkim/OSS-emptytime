4eca2e9737ff734a67062b33709af997e2b5cc2b
bibibic <star054321@naver.com>
2019-10-10 14:07:54 +0900

    urls관련 버그 추가수정
    사이트 접속 시, 홈이 아닌 시간표 합치기로 연결되는 버그를 추가로 수정하였습니다.

--- emptytime/emptytime/urls.py
+++ emptytime/emptytime/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notice.urls',)),
-    path('', include('timetable.urls')),
+    path('timetable/', include('timetable.urls')),
 ]
